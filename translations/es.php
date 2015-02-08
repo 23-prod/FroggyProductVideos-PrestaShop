749b411ee8925a6d80869b461e75858e13a4f846
Fabien Serny <fabien@23prod.fr>
2015-02-08 15:30:53 +0100

    Add translations files

---
+++ translations/es.php
@@ -0,0 +1,31 @@
+<?php
+
+global $_MODULE;
+
+$_MODULE = array();
+$_MODULE['<{froggyproductvideos}prestashop>froggyproductvideos_30f3456dc4f7f1be90968d2d58da8156'] = 'Froggy Vídeos Producto';
+$_MODULE['<{froggyproductvideos}prestashop>froggyproductvideos_482139e100f9ac15b7ceef9f64979b1c'] = 'Le permite añadir vídeos sobre su fichas productos';
+$_MODULE['<{froggyproductvideos}prestashop>froggyproductvideos_254f642527b45bc260048e30704edb39'] = 'Configuración';
+$_MODULE['<{froggyproductvideos}prestashop>froggyproductvideos_dae8ace18bdcbcc6ae5aece263e14fe8'] = 'Opciones';
+$_MODULE['<{froggyproductvideos}prestashop>froggyproductvideos_fd5817b52851910bbe63d521ba6eafae'] = 'Anchura de las vídeos:';
+$_MODULE['<{froggyproductvideos}prestashop>froggyproductvideos_41d605d27df423301bac8e044bf413e4'] = 'Altura de las vídeos:';
+$_MODULE['<{froggyproductvideos}prestashop>displaybackofficeheader_5329f68959b1775785f3a7906bd27a33'] = 'Vídeo Producto';
+$_MODULE['<{froggyproductvideos}prestashop>displayproducttab_5329f68959b1775785f3a7906bd27a33'] = 'Vídeo Producto';
+$_MODULE['<{froggyproductvideos}prestashop>displayproducttabcontent.bootstrap_5329f68959b1775785f3a7906bd27a33'] = 'Vídeo Producto';
+$_MODULE['<{froggyproductvideos}prestashop>getcontent.bootstrap_30f3456dc4f7f1be90968d2d58da8156'] = 'Froggy Vídeos Producto';
+$_MODULE['<{froggyproductvideos}prestashop>getcontent.bootstrap_4725c2b6db34f755b89b741c1d45bc71'] = '¡ La nueva configuración ha sido bien salvaguardada !';
+$_MODULE['<{froggyproductvideos}prestashop>getcontent_4725c2b6db34f755b89b741c1d45bc71'] = '¡ La nueva configuración ha sido bien salvaguardada !';
+$_MODULE['<{froggyproductvideos}prestashop>helper-form.bootstrap_93cba07454f06a4a960172bbd6e2a435'] = 'Sí';
+$_MODULE['<{froggyproductvideos}prestashop>helper-form.bootstrap_bafd7322c6e97d25b6299b5d6fe8920b'] = 'No';
+$_MODULE['<{froggyproductvideos}prestashop>helper-form.bootstrap_c9cc8cce247e49bae79f15173ce97354'] = 'Salvaguardar';
+$_MODULE['<{froggyproductvideos}prestashop>helper-form_93cba07454f06a4a960172bbd6e2a435'] = 'Sí';
+$_MODULE['<{froggyproductvideos}prestashop>helper-form_bafd7322c6e97d25b6299b5d6fe8920b'] = 'No';
+$_MODULE['<{froggyproductvideos}prestashop>helper-form_c9cc8cce247e49bae79f15173ce97354'] = 'Salvaguardar';
+$_MODULE['<{froggyproductvideos}prestashop>helper-list.bootstrap_06df33001c1d7187fdd81ea1f5b277aa'] = 'Acciones';
+$_MODULE['<{froggyproductvideos}prestashop>helper-list.bootstrap_f2a6c498fb90ee345d997f888fce3b18'] = 'Borrar';
+$_MODULE['<{froggyproductvideos}prestashop>helper-list_06df33001c1d7187fdd81ea1f5b277aa'] = 'Acciones';
+$_MODULE['<{froggyproductvideos}prestashop>helper-list_f2a6c498fb90ee345d997f888fce3b18'] = 'Borrar';
+$_MODULE['<{froggyproductvideos}prestashop>helper-tree.bootstrap_4c41e0bd957698b58100a5c687d757d9'] = 'Seleccionar todo';
+$_MODULE['<{froggyproductvideos}prestashop>helper-tree.bootstrap_237c7b6874386141a095e321c9fdfd38'] = 'Deseleccionar todo';
+$_MODULE['<{froggyproductvideos}prestashop>helper-tree_4c41e0bd957698b58100a5c687d757d9'] = 'Seleccionar todo';
+$_MODULE['<{froggyproductvideos}prestashop>helper-tree_237c7b6874386141a095e321c9fdfd38'] = 'Deseleccionar todo';
---
+++ translations/fr.php
@@ -0,0 +1,31 @@
+<?php
+
+global $_MODULE;
+
+$_MODULE = array();
+$_MODULE['<{froggyproductvideos}prestashop>froggyproductvideos_30f3456dc4f7f1be90968d2d58da8156'] = 'Froggy Videos Produit';
+$_MODULE['<{froggyproductvideos}prestashop>froggyproductvideos_482139e100f9ac15b7ceef9f64979b1c'] = 'Vous permet d\'ajouter des vidéos sur vos fiches produits';
+$_MODULE['<{froggyproductvideos}prestashop>froggyproductvideos_254f642527b45bc260048e30704edb39'] = 'Configuration';
+$_MODULE['<{froggyproductvideos}prestashop>froggyproductvideos_dae8ace18bdcbcc6ae5aece263e14fe8'] = 'Options';
+$_MODULE['<{froggyproductvideos}prestashop>froggyproductvideos_fd5817b52851910bbe63d521ba6eafae'] = 'Largeur des vidéos :';
+$_MODULE['<{froggyproductvideos}prestashop>froggyproductvideos_41d605d27df423301bac8e044bf413e4'] = 'Hauteur des vidéos :';
+$_MODULE['<{froggyproductvideos}prestashop>displaybackofficeheader_5329f68959b1775785f3a7906bd27a33'] = 'Vidéo produit';
+$_MODULE['<{froggyproductvideos}prestashop>displayproducttab_5329f68959b1775785f3a7906bd27a33'] = 'Vidéo produit';
+$_MODULE['<{froggyproductvideos}prestashop>displayproducttabcontent.bootstrap_5329f68959b1775785f3a7906bd27a33'] = 'Vidéo produit';
+$_MODULE['<{froggyproductvideos}prestashop>getcontent.bootstrap_30f3456dc4f7f1be90968d2d58da8156'] = 'Froggy Videos Produit';
+$_MODULE['<{froggyproductvideos}prestashop>getcontent.bootstrap_4725c2b6db34f755b89b741c1d45bc71'] = 'La nouvelle configuration a bien été sauvegardée !';
+$_MODULE['<{froggyproductvideos}prestashop>getcontent_4725c2b6db34f755b89b741c1d45bc71'] = 'La nouvelle configuration a bien été sauvegardée !';
+$_MODULE['<{froggyproductvideos}prestashop>helper-form.bootstrap_93cba07454f06a4a960172bbd6e2a435'] = 'Oui';
+$_MODULE['<{froggyproductvideos}prestashop>helper-form.bootstrap_bafd7322c6e97d25b6299b5d6fe8920b'] = 'Non';
+$_MODULE['<{froggyproductvideos}prestashop>helper-form.bootstrap_c9cc8cce247e49bae79f15173ce97354'] = 'Sauvegarder';
+$_MODULE['<{froggyproductvideos}prestashop>helper-form_93cba07454f06a4a960172bbd6e2a435'] = 'Oui';
+$_MODULE['<{froggyproductvideos}prestashop>helper-form_bafd7322c6e97d25b6299b5d6fe8920b'] = 'Non';
+$_MODULE['<{froggyproductvideos}prestashop>helper-form_c9cc8cce247e49bae79f15173ce97354'] = 'Sauvegarder';
+$_MODULE['<{froggyproductvideos}prestashop>helper-list.bootstrap_06df33001c1d7187fdd81ea1f5b277aa'] = 'Action';
+$_MODULE['<{froggyproductvideos}prestashop>helper-list.bootstrap_f2a6c498fb90ee345d997f888fce3b18'] = 'Effacer';
+$_MODULE['<{froggyproductvideos}prestashop>helper-list_06df33001c1d7187fdd81ea1f5b277aa'] = 'Action';
+$_MODULE['<{froggyproductvideos}prestashop>helper-list_f2a6c498fb90ee345d997f888fce3b18'] = 'Effacer';
+$_MODULE['<{froggyproductvideos}prestashop>helper-tree.bootstrap_4c41e0bd957698b58100a5c687d757d9'] = 'Tout sélectionner';
+$_MODULE['<{froggyproductvideos}prestashop>helper-tree.bootstrap_237c7b6874386141a095e321c9fdfd38'] = 'Tout désélectionner';
+$_MODULE['<{froggyproductvideos}prestashop>helper-tree_4c41e0bd957698b58100a5c687d757d9'] = 'Tout sélectionner';
+$_MODULE['<{froggyproductvideos}prestashop>helper-tree_237c7b6874386141a095e321c9fdfd38'] = 'Tout désélectionner';
